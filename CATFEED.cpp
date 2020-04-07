2125f6f1653bc047e3ebfae1b0c426ce942dc13b
prashanthsrn <46671599+Prashanthsrn@users.noreply.github.com>
2020-04-07 10:10:28 +0530

    Add files via upload

---
+++ CATFEED.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	int t = 0;
+	cin >> t ;
+	int k = 0;
+	
+	
+	
+	while(k < t){
+		int m, n;
+		cin >> n >> m;
+		int i = 0;
+		int check = 1;
+
+		int arr[m]; 
+		int count[n];
+		for(int i = 0; i < n; i++){
+			count[i] = 0;
+		}
+
+		for(int i = 0; i < m; i++){
+			cin >> arr[i];
+			count[arr[i] - 1]++;
+		
+			int temp = count[arr[i] - 1];
+
+			for(int j = 0; j < n; j++){
+				int temp2 = abs(temp - count[j]);
+				
+				if(temp2 == 2){
+					
+					check = 0;
+
+				}
+			}
+
+		}	
+		if(check == 1)
+			printf ("YES\n");
+		else
+			printf("NO\n");
+
+		k++;
+	}	
+}	
+
+
+
