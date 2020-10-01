14c1eaba0a1c353ced76606a27768c4515216b66
anku-13 <72180865+anku-13@users.noreply.github.com>
2020-10-01 09:45:38 +0530

    Update BRKBKS.cpp

--- BRKBKS.cpp
+++ BRKBKS.cpp
@@ -8,9 +8,9 @@ int main()
 	{
 	    int s, w1, w2, w3;
 	    cin >> s >> w1 >> w2 >> w3;
-	    if(s >= w1+w2+w3) cout << "1" << endl;
-	    else if((s >= w1+w2)||(s >= w2+w3)) cout << "2" << endl;
-	    else cout << "3" << endl;
+	    if(s >= w1+w2+w3) cout << "1" << "\n";
+	    else if((s >= w1+w2)||(s >= w2+w3)) cout << "2" <<  "\n";
+	    else cout << "3" << "\n";
 	}
 	return 0;
 }
